c12e9000bc5c5596b3dff7e26f99bdd26df84cd9
MuhammadAbdumalik <git config --global user.email your.email@example.com>
2026-01-28 19:14:19 +0500

    change font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,14 +21,14 @@ export default function Header({ onSignInClick, isAuthenticated, username }: Hea
   return (
     <header className="fixed top-0 left-0 right-0 z-50 w-full px-4 sm:px-8 md:px-16 lg:px-32 py-4 md:py-6 flex justify-between items-center backdrop-blur-md bg-white/80 border-b border-gray-200 shadow-xs">
       {/* Logo */}
-      <Link href="/" className="text-3xl sm:text-4xl md:text-5xl font-valisca" style={{
+      <Link href="/" className="text-3xl sm:text-4xl md:text-5xl font-moresugar pr-1" style={{
         background: 'linear-gradient(135deg, #E6007A 0%, #FF6600 100%)',
         WebkitBackgroundClip: 'text',
         WebkitTextFillColor: 'transparent',
         backgroundClip: 'text',
-
+        letterSpacing: '-0.1em',
       }}>
-        Weesh
+        weesh
       </Link>
 
       {/* Desktop Navigation */}
